3f5b4239096202377c505a6cb90a566f927156c3
Ramya Kottapalli <rskottap@gmail.com>
2024-10-23 14:41:44 -0700

    Change to get model_config based on module and keys for that module (like device, type) instead of trying to pass in the class name. Otherwise, messy imports, string parsing and not as re-usable.

--- src/airoot/test_load_model.py
+++ src/airoot/test_load_model.py
@@ -1,22 +1,55 @@
+#!/usr/bin/env python3
 """
 Tries to load the model into memory and run a sample generation.
 The internals of model init handle which device to fit to.
 """
 import argparse
 
-from airoot.audio import *
-
 
 def main():
     parser = argparse.ArgumentParser(description="test_load_model")
     parser.add_argument(
-        "model_config",
-        type=dict,
-        help="The model config to load {'model': <model class>, 'name': <model name>}.",
+        "-m",
+        "--module",
+        type=str,
+        required=True,
+        help="The module of airoot to test. One of audio, video, image and text.",
     )
-    parser.add_argument("text", type=str, help="Text prompt for sample generation.")
+    parser.add_argument(
+        "-k",
+        "--keys",
+        type=str,
+        nargs="*",
+        required=True,
+        help="The keys IN ORDER, to index into the modules 'model config' dictionary to get the list of default models. For ex, 'cpu' and 'speech' to test the audio models for cpu, for speech to text.",
+    )
+    parser.add_argument(
+        "--idx",
+        type=int,
+        default=0,
+        help="The index of the model in the available list of default models for a given config.",
+    )
+    parser.add_argument(
+        "-t",
+        "--text",
+        type=str,
+        required=True,
+        help="Text prompt for sample generation.",
+    )
     args = parser.parse_args()
 
-    model = args.model_config
-    m = model["model"](name=model["name"])
-    _ = m.generate(args.text)
+    if args.module == "audio":
+        from airoot import audio
+
+        model_config = audio.get_models()
+        model = model_config
+        for key in args.keys:
+            model = model[key]
+        model = model[args.idx]
+
+        m = model["model"](name=model["name"])
+        # _ = m.generate(args.text)
+
+
+if __name__ == "__main__":
+    main()

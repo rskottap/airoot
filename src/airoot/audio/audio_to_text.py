2e0dbca4bd033b8e5290071142b6f6fb601a8bca
Ramya Kottapalli <rskottap@gmail.com>
2024-10-28 12:08:10 -0700

    Add Whisper2 via pipeline to process >30 sec samples

--- src/airoot/audio/audio_to_text.py
+++ src/airoot/audio/audio_to_text.py
@@ -1,6 +1,7 @@
 __all__ = [
     "AudioToText",
     "Whisper",
+    "Whisper2",
 ]
 
 import json
@@ -8,7 +9,7 @@ import logging
 import subprocess
 
 import torch
-from transformers import WhisperForConditionalGeneration, WhisperProcessor
+from transformers import WhisperForConditionalGeneration, WhisperProcessor, pipeline
 
 from airoot.base_model import BaseModel, get_default_model, set_default_model
 
@@ -17,6 +18,7 @@ logger = logging.getLogger("airoot.AudioToText")
 
 class Whisper(BaseModel):
     # for speech to text
+    # The Whisper model is intrinsically designed to work on audio samples of up to 30s in duration.
     # name="openai/whisper-large-v2" # name="openai/whisper-medium" # name="openai/whisper-small"
 
     def __init__(self, name="openai/whisper-small"):
@@ -31,6 +33,7 @@ class Whisper(BaseModel):
         self.model.to(self.device)
 
     def generate(self, data, sample_rate, language=None, task="transcribe"):
+        # If language or task is not provided, automatically detects source language and translates to English
         if language is not None:
             forced_decoder_ids = self.processor.get_decoder_prompt_ids(
                 language=language, task=task
@@ -48,12 +51,51 @@ class Whisper(BaseModel):
         return text
 
 
+class Whisper2(BaseModel):
+    # for speech to text
+    # For samples longer than 30 seconds use the chunking algorithm via the "automatic-speech-recognition" pipeline
+    # name="openai/whisper-large-v2" # name="openai/whisper-medium" # name="openai/whisper-small"
+
+    def __init__(self, name="openai/whisper-small"):
+        super().__init__()
+        self.name = name
+        self.load_model()
+
+    def load_model(self):
+        self.pipe = pipeline(
+            "automatic-speech-recognition",
+            model=self.name,
+            chunk_length_s=30,
+            device=self.device,
+        )
+        self.processor = WhisperProcessor.from_pretrained(self.name)
+        self.original_decoder_ids = self.pipe.model.generation_config.forced_decoder_ids
+
+    def generate(self, data, task="transcribe"):
+        # Due to a bug fix in https://github.com/huggingface/transformers/pull/28687 transcription using a multilingual Whisper will default to language detection followed by transcription instead of translation to English.
+        # If you want to instead always translate your audio to English, make sure to pass `language='en'`
+        if task == "translate":
+            forced_decoder_ids = self.processor.get_decoder_prompt_ids(
+                language="english", task=task
+            )
+            self.pipe.model.generation_config.forced_decoder_ids = forced_decoder_ids
+            text = self.pipe(data)["text"]
+            # reset back for next generations
+            self.pipe.model.generation_config.forced_decoder_ids = (
+                self.original_decoder_ids
+            )
+        else:
+            # by default detects language and transcribes into that language
+            text = self.pipe(data)["text"]
+        return text
+
+
 config = {
     "cpu": [
-        {"model": Whisper, "name": "openai/whisper-medium"},
-        {"model": Whisper, "name": "openai/whisper-small"},
+        {"model": Whisper2, "name": "openai/whisper-medium"},
+        {"model": Whisper2, "name": "openai/whisper-small"},
     ],
-    "cuda": [{"model": Whisper, "name": "openai/whisper-large-v2"}],
+    "cuda": [{"model": Whisper2, "name": "openai/whisper-large-v2"}],
 }
 
 

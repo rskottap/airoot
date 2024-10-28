f4a42e2ed5b11b236195b621de7bd7da2baaa02c
Ramya Kottapalli <rskottap@gmail.com>
2024-10-28 12:09:13 -0700

    Remove language option since it auto-detects and fix piping out text. Tested CLI.

--- src/airoot/bin/audiototext.py
+++ src/airoot/bin/audiototext.py
@@ -18,21 +18,14 @@ def parse_args():
         "audio_file_path",
         nargs="?",
         type=str,
-        help="Path to audio file to transcribe/translate (.wav files recommended)",
-    )
-    parser.add_argument(
-        "-l",
-        "--language",
-        type=str,
-        default=None,
-        help="Language of source audio. If not given, auto-infers source audio and TRANSLATES from source audio language to English. If given, by DEFAULT, TRANSCRIBES to text in same language as audio file.",
+        help="Path to audio file (.wav files recommended!)",
     )
     parser.add_argument(
         "-t",
         "--task",
         choices=["transcribe", "translate"],
         default="transcribe",
-        help="Task to perform: 'transcribe' or 'translate' (default: 'transcribe'). If language is specified, for transcription, converts audio in language x to text in x. For translation converts audio in language x to text in english.",
+        help="Task to perform: 'transcribe' or 'translate' (default: 'transcribe').\nAuto-detects language, and based on the task, either transcribes audio to text in that language or translates to English.",
     )
     parser.add_argument(
         "-o",
@@ -54,32 +47,33 @@ def main():
         audio = sys.stdin.read()
         audio, sr = librosa.load(audio, sr=target_sr)
     else:
+        # Raise error if no audio input is provided
+        if not args.audio_file_path:
+            raise Exception(
+                "Error: No audio input provided. Provide audio_file_path as a positional argument (or pipe audio bytes into the command)."
+            )
         if not args.audio_file_path.endswith("wav"):
             logger.warning(
                 f"Recommended to use .wav audio files for input. Gave {args.audio_file_path}"
             )
 
-        audio, sr = librosa.load(args.audio_file_path, sr=target_sr)
+        audio, sr = librosa.load(args.audio_file_path.strip(), sr=target_sr)
 
-    # Raise error if no audio input is provided
-    if not audio:
-        raise Exception(
-            "Error: No audio input provided. Provide audio_file_path as a positional argument or pipe an audio bytes into the command."
-        )
     model = AudioToText()
-    text = model.generate(audio, sr, language=args.language, task=args.task)
+    text = model.generate(audio, task=args.task) + "\n"
 
     # Write to output file if provided
     if args.output:
         with open(args.output.strip(), "w") as f:
             f.write(text)
 
-    # If stdout is not connected to a terminal (i.e., there's a pipe), output the text as raw bytes
+    # If stdout is not connected to a terminal (i.e., there's a pipe), pipe the text
     if not sys.stdout.isatty():
-        sys.stdout.buffer.write(text.tobytes())
+        sys.stdout.buffer.write(text.encode())
     else:
         # no pipe and output file not provided, then print to stdout
         if not args.output:
+            logger.info("Output text is:")
             print(text)
 
 
